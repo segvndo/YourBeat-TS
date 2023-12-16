0c21d161b99ad2c4f0733559b006155e202cab19
Bea Mungcal <beamungcal@hotmail.com>
2023-12-15 19:25:17 -0500

    Remove parts of functions in the text area logic that are not being used

--- app/utilities/useTextareaSizeLogic.tsx
+++ app/utilities/useTextareaSizeLogic.tsx
@@ -6,14 +6,13 @@ interface TextareaSizeLogic {
   textareaValue: string;
   isPlaceholderVisible: boolean;
   setIsPlaceholderVisible: React.Dispatch<React.SetStateAction<boolean>>;
-  setTextareaValue: React.Dispatch<React.SetStateAction<string>>;
   handleTextareaSizeChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
   isResizing: boolean;
+  textareaRef: React.RefObject<HTMLTextAreaElement>;
+  setTextareaValue: React.Dispatch<React.SetStateAction<string>>;
   handleMouseDown: (e: React.MouseEvent) => void;
   handleMouseMove: (e: React.MouseEvent) => void;
   handleMouseUp: () => void;
-  textareaRef: React.RefObject<HTMLTextAreaElement>;
-  setHasStartedTyping: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const useTextareaSizeLogic = (): TextareaSizeLogic => {
@@ -22,19 +21,13 @@ export const useTextareaSizeLogic = (): TextareaSizeLogic => {
   const [isResizing, setIsResizing] = useState<boolean>(false);
   const [textareaValue, setTextareaValue] = useState<string>('Provide a brief description');
   const [isPlaceholderVisible, setIsPlaceholderVisible] = useState<boolean>(true);
-  const [hasStartedTyping, setHasStartedTyping] = useState<boolean>(false);
-
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const startX = useRef<number>(0);
-  const startY = useRef<number>(0);
-
 
   const handleMouseDown = (e: React.MouseEvent) => {
     const textareaRect = textareaRef.current?.getBoundingClientRect();
     const rightBottomCornerX = textareaRect?.right || 0;
     const rightBottomCornerY = textareaRect?.bottom || 0;
-    const resizeArea = 10;
   
     if (
       e.clientX >= rightBottomCornerX - 10 &&
@@ -43,22 +36,18 @@ export const useTextareaSizeLogic = (): TextareaSizeLogic => {
       e.clientY <= rightBottomCornerY
     ) {
       setIsResizing(true);
-      startX.current = e.clientX;
-      startY.current = e.clientY;
+
     }
   };
   
-
   const handleMouseMove = (e: React.MouseEvent) => {
     if (isResizing) {
-      const deltaX = e.clientX - startX.current;
-      const deltaY = e.clientY - startY.current;
+      const deltaX = e.clientX - (textareaRef.current?.getBoundingClientRect()?.right || 0);
+      const deltaY = e.clientY - (textareaRef.current?.getBoundingClientRect()?.bottom || 0);
 
       setTextareaWidth(`${parseFloat(textareaWidth) + deltaX}px`);
       setTextareaHeight(`${parseFloat(textareaHeight) + deltaY}px`);
 
-      startX.current = e.clientX;
-      startY.current = e.clientY;
     }
   };
 
