21ab7addb9667efa724505f0eea0b7ed078372b5
Bea Mungcal <beamungcal@hotmail.com>
2023-12-18 22:10:14 -0500

    Remove dropdown option from venueDetails and replace with builtin Tailwind styling and dropdown

--- app/users/venue/venueDetails/page.tsx
+++ app/users/venue/venueDetails/page.tsx
@@ -95,7 +95,7 @@ const VenueDetails = () => {
         ></textarea>
       </div>
 
-      <div className="flex items-center relative">
+      {/* <div className="flex items-center relative">
         <p className="mr-4 bg-white text-black p-2">Choose Venue Type</p>
         <button
           onClick={toggleDropdown}
@@ -132,7 +132,19 @@ const VenueDetails = () => {
         <div className="mt-4">
           <p>Selected Venue: {selectedOption}</p>
         </div>
-      )}
+      )} */}
+
+  <div class="relative inline-block text-left">
+  <div>
+    <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+      Choose Venue Type
+      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+      </svg>
+    </button>
+    </div>
+  </div>
+
     </div>
   );
 };
