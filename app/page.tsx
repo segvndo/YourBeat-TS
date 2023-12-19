ffd31e1a41b71fd65ca06a2341cd695b76937c9c
Bea Mungcal <beamungcal@hotmail.com>
2023-12-19 01:17:21 -0500

    Add card details to venue card in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -12,24 +12,40 @@ export default function Home() {
       <div className="font-sans text-lg text-red-500">
         About YourBeat
       </div>
-        <div className="font-sans font-thin brightness-">
-          YourBeat is a Social Media platform which strives to connect Musical Artists, Vendors, Venues, and Fans. By assisting artists in building a strong community of thousands of dedicated fans and connecting them with vendors and venues, facilitating mutually beneficial business relationships, we strive to grow a fully sustainable environment and revolutionizing the music industry.
-        </div>
+      <div className="font-sans font-thin brightness-">
+        YourBeat is a Social Media platform which strives to connect Musical Artists, Vendors, Venues, and Fans. By assisting artists in building a strong community of thousands of dedicated fans and connecting them with vendors and venues, facilitating mutually beneficial business relationships, we strive to grow a fully sustainable environment and revolutionizing the music industry.
+      </div>
 
-        <div className="flex space-x-4">
-          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
-            Venue
-          </div>
-          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
-            Vendor
-          </div>
-          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
-            Artist
-          </div>
-          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
-            Fan
+      <div className="flex space-x-4">
+        <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex flex-col items-center font-sans">
+          <div className="text-red-500 font-sans mb-4">Venue</div>
+          <div>
+            <ul className="text-white pl-4 list-disc text-center">
+              <li>
+                Book artists for live shows
+              </li>
+              <li>
+                Manage event details
+              </li>
+              <li>
+                Manage venue details
+              </li>
+              <li>
+                Coordinate with vendors
+              </li>
+            </ul>
           </div>
         </div>
+        <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center">
+          <div className="text-red-500 font-sans">Vendor</div>
+        </div>
+        <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center">
+          <div className="text-red-500 font-sans">Artist</div>
+        </div>
+        <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center">
+          <div className="text-red-500 font-sans">Fan</div>
+        </div>
+      </div>
 
     </main>
   )
