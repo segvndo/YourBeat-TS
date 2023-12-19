84a65a86fa1fd13de36dce4216e410592ba50f46
Bea Mungcal <beamungcal@hotmail.com>
2023-12-18 23:56:56 -0500

    Modify tailwind.config

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div class="font-bold text-lg">
         About YourBeat
       </div>
-        <div class="font-sans hover:font-serif">
+        <div class="font-sans">
           YourBeat is a Social Media platform which strives to connect Musical Artists, Vendors, Venues, and Fans. By assisting artists in building a strong community of thousands of dedicated fans and connecting them with vendors and venues, facilitating mutually beneficial business relationships, we strive to grow a fully sustainable environment and revolutionizing the music industry.
         </div>
 
