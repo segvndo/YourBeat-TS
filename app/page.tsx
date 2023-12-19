f6779f92a6ff2fe1512b14a6a342550062ad8a93
Bea Mungcal <beamungcal@hotmail.com>
2023-12-19 00:57:15 -0500

    Multiply cards on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,18 @@ export default function Home() {
           YourBeat is a Social Media platform which strives to connect Musical Artists, Vendors, Venues, and Fans. By assisting artists in building a strong community of thousands of dedicated fans and connecting them with vendors and venues, facilitating mutually beneficial business relationships, we strive to grow a fully sustainable environment and revolutionizing the music industry.
         </div>
 
-        <div>
-          <div className="box-content h-32 w-32 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
+        <div className="flex space-x-4">
+          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
             Venue
+          </div>
+          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
+            Vendor
+          </div>
+          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
+            Artist
+          </div>
+          <div className="box-content h-96 w-64 p-4 border-4 bg-gray-600 flex items-start justify-center text-red-500 font-sans">
+            Fan
           </div>
         </div>
 
