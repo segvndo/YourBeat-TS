a32d254b7af1f3efbcd5c36a7dcfba6d7fec922a
Bea Mungcal <beamungcal@hotmail.com>
2023-12-19 00:25:29 -0500

    Style About YourBeat on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,13 +9,19 @@ export default function Home() {
       </h1>
 
 
-      <div class="font-bold text-lg">
+      <div className="font-sans text-lg text-red-500">
         About YourBeat
       </div>
-        <div class="font-sans">
+        <div className="font-sans font-thin brightness-">
           YourBeat is a Social Media platform which strives to connect Musical Artists, Vendors, Venues, and Fans. By assisting artists in building a strong community of thousands of dedicated fans and connecting them with vendors and venues, facilitating mutually beneficial business relationships, we strive to grow a fully sustainable environment and revolutionizing the music industry.
+        </div>
+
+        <div>
+          <div>
+          Venue
+          </div>
         </div>
 
     </main>
   )
-}
+};
