827a4817a4239852ce08ba48078b500a1832761f
Bea Mungcal <beamungcal@hotmail.com>
2023-12-19 02:56:23 -0500

    Reposition mailing list types and subscription button

--- app/page.tsx
+++ app/page.tsx
@@ -111,12 +111,27 @@ export default function Home() {
           YourBeat is an exciting new music network platform with a strong commitment to personal data security. Sign up for out mailing list to get notified when the web app is deployed and we'll be sure to let you know when you can create your profile and start connecting with music industry professionals or discovering awesome new artists!
         </div>
 
-        <div className="flex justify-center items-center">
-          <input className="h-10" type="text" placeholder="Email Address" />
+        <div className="flex flex-row justify-center">
           <div>
-              <button className="box-content h-15 w-15 p-2 bg-green-500 rounded-lg m-2">Subscribe</button>
-            </div>
+            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Venue</button>
+          </div>
+          <div>
+            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Artist</button>
+          </div>
+          <div>
+            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Vendor</button>
+          </div>
+          <div>
+            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Fan</button>
+          </div>
         </div>
+
+        <div className="flex justify-center items-center">
+          <input className="h-10 mt-4 pl-2" type="text" placeholder="Email Address" />
+        </div>
+          <div className="flex justify-center items-center mt-3">
+              <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Subscribe</button>
+            </div>
       </footer>
 
     </main>
