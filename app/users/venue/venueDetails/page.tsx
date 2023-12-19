7da3587929074c4387b7f4ba5e3b5b0360a0dd5e
Bea Mungcal <beamungcal@hotmail.com>
2023-12-18 22:46:58 -0500

    Reapply previously unused codes

--- app/users/venue/venueDetails/page.tsx
+++ app/users/venue/venueDetails/page.tsx
@@ -1,12 +1,11 @@
 'use client';
-import React from 'react'
+import React from 'react';
 import { useTextareaSizeLogic } from '@/app/utilities/useTextareaSizeLogic';
-// import { useDropdownLogic } from '@/app/utilities/useDropdownLogic';
+import { useDropdownLogic } from '@/app/utilities/useDropdownLogic';
 
 const VenueDetails = () => {
   const {
     textareaWidth,
-    textareaHeight,
     handleTextareaSizeChange,
     isResizing,
     textareaRef,
@@ -19,62 +18,50 @@ const VenueDetails = () => {
     setIsPlaceholderVisible,
   } = useTextareaSizeLogic();
 
-  // const {
-  //   isOpen,
-  //   toggleDropdown,
-  //   selectedOption,
-  //   selectOption,
-  //   closeDropdown,
-  //   dropdownRef,
-  // } = useDropdownLogic();
+  const {
+    isOpen,
+    toggleDropdown,
+    selectedOption,
+    selectOption,
+    dropdownRef,
+  } = useDropdownLogic();
 
-  // const venueOptions = [
-  //   'Concert Hall',
-  //   'Amphitheater',
-  //   'Stadium/Arena',
-  //   'Club/Bar',
-  //   'Coffeehouse/Listening Room',
-  //   'Warehouse Space',
-  //   'EDM Venue',
-  //   'Cabaret/Lounge',
-  // ];
-
+  const venueOptions = [
+    'Concert Hall',
+    'Amphitheater',
+    'Stadium/Arena',
+    'Club/Bar',
+    'Coffeehouse/Listening Room',
+    'Warehouse Space',
+    'EDM Venue',
+    'Cabaret/Lounge',
+  ];
 
   return (
     <div>
-      <h1 className="font-bold">
-        Venue Details
-      </h1>
+      <h1 className="font-bold">Venue Details</h1>
 
       <div>
         <p>Venue Name</p>
-        <input
-          type="text"
-          name="venueName"
-        />
+        <input type="text" name="venueName" />
       </div>
 
       <div>
         <p>Maximum Capacity</p>
-        <input
-          type="text"
-          name="venueCapacity"
-        />
+        <input type="text" name="venueCapacity" />
       </div>
 
       <div>
         <p>Venue Description</p>
-        
         <textarea
           ref={textareaRef}
           name="venueDescription"
           maxLength={300}
           value={textareaValue}
           onFocus={() => {
-              setTextareaValue('');
-              setIsPlaceholderVisible(false);
-            }
-          }
+            setTextareaValue('');
+            setIsPlaceholderVisible(false);
+          }}
           onBlur={() => {
             if (textareaValue === '') {
               setTextareaValue('Provide a brief description');
@@ -95,72 +82,59 @@ const VenueDetails = () => {
         ></textarea>
       </div>
 
-      {/* <div className="flex items-center relative">
-        <p className="mr-4 bg-white text-black p-2">Choose Venue Type</p>
+      <div className="relative inline-block text-left">
         <button
+          type="button"
+          className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
           onClick={toggleDropdown}
-          className="flex items-center px-3 py-2 bg-white text-black rounded"
         >
-          <span className="ml-2">&#9660;</span>
+          Choose Venue Type
+          <svg
+            className="-mr-1 h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
         </button>
 
         {isOpen && (
           <div
             ref={dropdownRef}
-            className="absolute top-full left-0 bg-white shadow-md p-4 rounded border border-t-0 border-r-0 border-l-0"
-            style={{ zIndex: 10 }}
+            className="absolute origin-top-left mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button"
+            tabIndex="-1"
           >
-            <p className="font-bold mb-2">Select Venue</p>
-            <ul className="list-none p-0 m-0">
-              {venueOptions.map((option) => (
-                <li
-                  key={option}
-                  onClick={() => selectOption(option)}
-                  className={`cursor-pointer text-black hover:bg-gray-200 ${
-                    selectedOption === option ? 'text-yellow-500' : ''
+            <div className="py-1" role="none">
+              {/* Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" */}
+              {venueOptions.map((option, index) => (
+                <a
+                  key={index}
+                  href="#"
+                  className={`text-gray-700 block px-4 py-2 text-sm ${
+                    selectedOption === option ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
                   }`}
+                  role="menuitem"
+                  tabIndex="-1"
+                  onClick={() => selectOption(option)}
                 >
                   {option}
-                </li>
+                </a>
               ))}
-            </ul>
+            </div>
           </div>
         )}
       </div>
-
-      {selectedOption && (
-        <div className="mt-4">
-          <p>Selected Venue: {selectedOption}</p>
-        </div>
-      )} */}
-
-  <div class="relative inline-block text-left">
-  <div>
-    <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-      Choose Venue Type
-      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-      </svg>
-    </button>
-    </div>
-  </div>
-
-  <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-    <div class="py-1" role="none">
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Concert Hall</a>
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Amphitheatre</a>
-    </div>
-    <div class="py-1" role="none">
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Stadium/Arena</a>
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">About</a>
-    </div>
-    <div class="py-1" role="none">
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-4">Club/Bar</a>
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-5">Warehouse Space</a>
-    </div>
-  </div>
-
-
     </div>
   );
 };
