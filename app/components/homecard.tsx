69528bfaf62fb0bb324ae12c5326032cd19b33d8
Bea Mungcal <beamungcal@hotmail.com>
2023-12-21 00:19:17 -0500

    Add border colour to home card

--- app/components/homecard.tsx
+++ app/components/homecard.tsx
@@ -8,13 +8,15 @@ interface CardProps {
   buttonText: string;
   buttonLink: string;
   mainBackgroundColour?: string; // Optional background color prop
+  borderColour?: string;
+  titleColour?: string
 }
 
-const Card: React.FC<CardProps> = ({ title, description, buttonText, buttonLink, mainBackgroundColour }) => {
+const Card: React.FC<CardProps> = ({ title, description, buttonText, buttonLink, mainBackgroundColour, borderColour }) => {
   return (
-    <div className="box-content h-115 w-72 bg-green-500 pt-16 rounded-lg">
-      <div className={`box-content h-96 w-54 p-4 border-4 flex flex-col items-center font-sans ${mainBackgroundColour}`}>
-        <div className="text-red-500 font-sans mb-4 font-semibold text-3xl">{title}</div>
+    <div className="box-content h-115 w-72 bg-green-500 pt-5 rounded-lg">
+      <div className="text-red-500 font-sans mb-4 font-semibold text-3xl flex justify-center">{title}</div>
+      <div className={`box-content h-96 w-54 p-4 border-4 ${borderColour || 'border-green-400'} flex flex-col items-center font-sans ${mainBackgroundColour}`}>
         <div className="flex-grow">
           <ul className="text-white pl-4 list-disc text-center font-extralight">{description.map((item, index) => (
             <li key={index}>{item}</li>
--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,7 @@ export default function Home() {
           buttonText="Learn more"
           buttonLink="/users/venue"
           mainBackgroundColour="bg-green-600">
+          borderColour="border-green-600"
         </Card>
 
 
