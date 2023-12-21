d16fd9610bfbb62cbf96f8a6e7f3f42dc0a24648
Bea Mungcal <beamungcal@hotmail.com>
2023-12-20 22:43:56 -0500

    Remove previous footer codes from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -105,38 +105,6 @@ export default function Home() {
 
       <Mailing />
 
-      {/* <footer>
-        <div className="flex justify-center items-center mt-6 text-red-500 font-sans mb-4 text-xl">
-          Sign Up For YourBeat Mailing List
-        </div>
-
-        <div>
-          YourBeat is an exciting new music network platform with a strong commitment to personal data security. Sign up for out mailing list to get notified when the web app is deployed and we'll be sure to let you know when you can create your profile and start connecting with music industry professionals or discovering awesome new artists!
-        </div>
-
-        <div className="flex flex-row justify-center">
-          <div>
-            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Venue</button>
-          </div>
-          <div>
-            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Artist</button>
-          </div>
-          <div>
-            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Vendor</button>
-          </div>
-          <div>
-            <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Fan</button>
-          </div>
-        </div>
-
-        <div className="flex justify-center items-center">
-          <input className="h-10 mt-4 pl-2" type="text" placeholder="Email Address" />
-        </div>
-          <div className="flex justify-center items-center mt-3">
-              <button className="box-content h-10 w-32 p-2 bg-green-500 rounded-lg m-2">Subscribe</button>
-            </div>
-      </footer> */}
-
     </main>
   )
 };
