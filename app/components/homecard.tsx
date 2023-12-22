53be7fe617584a5bc48e6d2716aca65716561f52
Bea Mungcal <beamungcal@hotmail.com>
2023-12-21 23:44:38 -0500

    Center text on card

--- app/components/homecard.tsx
+++ app/components/homecard.tsx
@@ -13,16 +13,23 @@ type CardProps = {
 const Card: FC<CardProps> = ({ title, description, buttonText, buttonLink, mainBackgroundColour, borderColour }) => {
   return (
     <div className="box-content h-115 w-64 pt-6">
-      <div className={`box-content h-96 w-64 p-4 border-4 flex flex-col items-center font-sans ${mainBackgroundColour}`}>
-        <div>Hello</div>
-        <div className="text-yellow-50 font-sans mb-4 font-semibold text-3xl">{title}</div>
-        <div className="flex-grow bg-gray-800 rounded-md p-4">
-          <ul className="text-yellow-50 pl-4 list-disc text-center font-extralight">
+      <div className={`box-content h-96 w-64 p-4 flex flex-col items-center font-sans ${mainBackgroundColour}`}>
+
+
+        <div className="box-content h-96 w-72 rounded-lg bg-green-800 text-yellow-50 font-sans mb-4 font-semibold text-3xl text-center">
+          
+          {title}
+          
+        <div className="flex-grow bg-gray-800 rounded-md p-4 pb-40 text-lg">
+          <ul className="text-yellow-50 pl-6 list-disc text-center font-extralight">
             {description.map((item, index) => (
               <li key={index}>{item}</li>
             ))}
           </ul>
         </div>
+        </div>
+
+
       </div>
     </div>
   );
