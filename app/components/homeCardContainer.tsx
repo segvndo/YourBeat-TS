90b9bb3c1f9a8564be056955639005dc8a019e9b
Bea Mungcal <beamungcal@hotmail.com>
2023-12-22 02:06:52 -0500

    Create fan, vendor, and venue cards

--- app/components/homeCardContainer.tsx
+++ app/components/homeCardContainer.tsx
@@ -21,21 +21,48 @@ const HomeCardContainer: React.FC<HomeCardContainerProps> = () => {
         listBorderColour="border-green-500">
       </Card>
 
-      <div>
-        <Card
-          title="Fan"
-          description={[
-            'Discover new artists',
-            'Connect with artists',
-            'Get recommendations for other artists',
-            'Purchase merchandise',
-            'Purchase tickets'
-          ]}
-          mainBackgroundColour="bg-red-500"
-          secondaryBackgroundColour="bg-red-800"
-          listBorderColour="border-red-500">
-        </Card>
-      </div>
+      <Card
+        title="Fan"
+        description={[
+          'Discover new artists',
+          'Connect with artists',
+          'Get recommendations for other artists',
+          'Purchase merchandise',
+          'Purchase tickets'
+        ]}
+        mainBackgroundColour='bg-yellow-500'
+        secondaryBackgroundColour='bg-yellow-800'
+        listBorderColour='border-yellow-500'>
+      </Card>
+
+      <Card
+        title="Vendor"
+        description={[
+          'Connect with artists',
+          'Connect with venues',
+          'Sell merchandise',
+          'Sell tickets',
+          'Advertise events'
+        ]}
+        mainBackgroundColour="bg-orange-500"
+        secondaryBackgroundColour="bg-orange-800"
+        listBorderColour="border-orange-500">
+      </Card>
+
+      <Card
+        title="Venue"
+        description={[
+          'Connect with artists',
+          'Connect with vendors',
+          'Sell tickets',
+          'Sell merchandise',
+          'Advertise events'
+        ]}
+        mainBackgroundColour="bg-red-500"
+        secondaryBackgroundColour="bg-red-800"
+        listBorderColour="border-red-500">
+      </Card>
+
 
     </div>
   )
