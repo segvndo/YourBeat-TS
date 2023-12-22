84267f237eca3055a3b899b645f9a1d6abd4a613
Bea Mungcal <beamungcal@hotmail.com>
2023-12-22 01:43:06 -0500

    Import card section to home

--- app/components/homeCardContainer.tsx
+++ app/components/homeCardContainer.tsx
@@ -4,40 +4,40 @@ import Card from '@/app/components/homeCard';
 type HomeCardContainerProps = {};
 
 const HomeCardContainer: React.FC<HomeCardContainerProps> = () => {
-  
+
   return (
     <div className="flex flex-row space-x-6">
-          <Card
-            title="Artist"
-            description={[
-              'Connect with fans',
-              'Sell merchandise',
-              'Sell tickets',
-              'Connect with vendors',
-              'Connect with venues'
-            ]}
-            mainBackgroundColour="bg-green-500"
-            secondaryBackgroundColour="bg-green-800"
-            listBorderColour="border-green-500">
-          </Card>
+      <Card
+        title="Artist"
+        description={[
+          'Connect with fans',
+          'Sell merchandise',
+          'Sell tickets',
+          'Connect with vendors',
+          'Connect with venues'
+        ]}
+        mainBackgroundColour="bg-green-500"
+        secondaryBackgroundColour="bg-green-800"
+        listBorderColour="border-green-500">
+      </Card>
 
 
-          <Card
-            title="Fan"
-            description={[
-              'Discover new artists',
-              'Connect with artists',
-              'Get recommendations for other artists',
-              'Purchase merchandise',
-              'Purchase tickets'
-            ]}
-            mainBackgroundColour="bg-red-500"
-            secondaryBackgroundColour="bg-red-800"
-            listBorderColour="border-red-500">
-          </Card>
+      <Card
+        title="Fan"
+        description={[
+          'Discover new artists',
+          'Connect with artists',
+          'Get recommendations for other artists',
+          'Purchase merchandise',
+          'Purchase tickets'
+        ]}
+        mainBackgroundColour="bg-red-500"
+        secondaryBackgroundColour="bg-red-800"
+        listBorderColour="border-red-500">
+      </Card>
 
 
-        </div>
+    </div>
   )
 
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Mailing from './components/mailing';
 import Card from "./components/homecard";
+import HomeCardContainer from './components/homeCardContainer';
 
 export default function Home() {
   return (
@@ -19,37 +20,8 @@ export default function Home() {
           YourBeat is a Social Media platform which strives to connect Musical Artists, Vendors, Venues, and Fans. By assisting artists in building a strong community of thousands of dedicated fans and connecting them with vendors and venues, facilitating mutually beneficial business relationships, we strive to grow a fully sustainable environment and revolutionizing the music industry.
         </div>
 
-        <div className="flex flex-row space-x-6">
-          <Card
-            title="Artist"
-            description={[
-              'Connect with fans',
-              'Sell merchandise',
-              'Sell tickets',
-              'Connect with vendors',
-              'Connect with venues'
-            ]}
-            mainBackgroundColour="bg-green-500"
-            secondaryBackgroundColour="bg-green-800"
-            listBorderColour="border-green-500">
-          </Card>
-
-
-          <Card
-            title="Fan"
-            description={[
-              'Discover new artists',
-              'Connect with artists',
-              'Get recommendations for other artists',
-              'Purchase merchandise',
-              'Purchase tickets'
-            ]}
-            mainBackgroundColour="bg-red-500"
-            secondaryBackgroundColour="bg-red-800"
-            listBorderColour="border-red-500">
-          </Card>
-
-
+        <div>
+          <HomeCardContainer />
         </div>
 
 
