951f9fd6b1a413304f2b9fd6fbd13006ea9060ab
Bea Mungcal <beamungcal@hotmail.com>
2023-12-23 22:53:21 -0500

    Create handleSubscribe in mailing component

--- app/components/mailing.tsx
+++ app/components/mailing.tsx
@@ -1,32 +1,44 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import Button from "@/app/components/button";
 
 const Mailing: React.FC = () => {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [email, setEmail] = useState<string>('');
+
+  const handleSubscribe = async () => {
+    try {
+      setIsSubscribed(true);
+    } catch (error) {
+      console.error('Error subscribing to mailing list:', error);
+    }
+  };
+
   return (
-    <footer>
-      <div className="flex justify-center items-center mt-40 text-red-500 font-sans mb-4 text-xl">
-        Sign Up For YourBeat Mailing List
-      </div>
-
+    <div>
       <div>
-        YourBeat is an exciting new music network platform with a strong commitment to personal data security. Sign up for our mailing list to get notified when the web app is deployed, and we&apos;ll be sure to let you know when you can create your profile and start connecting with music industry professionals or discovering awesome new artists!
+        YourBeat is an exciting new music network platform with a strong commitment to personal data security. Sign up for our mailing list to get notified when the web app is deployed, and we'll be sure to let you know when you can create your profile and start connecting with music industry professionals or discovering awesome new artists!
       </div>
-
-      <div className="flex flex-row justify-center">
-        <Button variant="green">Artist</Button>
-        <Button>Fan</Button>
-        <Button>Vendor</Button>
-        <Button>Venue</Button>
-      </div>
-
-      <div className="flex justify-center items-center">
-        <input className="h-10 mt-4 pl-2" type="text" placeholder="Email Address" />
-      </div>
-
-      <div className="flex justify-center items-center mt-3">
-        <Button>Subscribe</Button>
-      </div>
-    </footer>
+      {!isSubscribed ? (
+        <>
+          <Button variant="green">Option 1</Button>
+          <Button variant="green">Option 2</Button>
+          <Button variant="green">Option 3</Button>
+          <Button variant="green">Option 4</Button>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Button variant="green" onClick={handleSubscribe}>
+            Subscribe
+          </Button>
+        </>
+      ) : (
+        <p>Thank you for signing up!</p>
+      )}
+    </div>
   );
 };
 
