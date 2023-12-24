c9661393ad72c4e3a0a321737c5cb2a918773b36
Bea Mungcal <beamungcal@hotmail.com>
2023-12-23 22:54:19 -0500

    Remove promise in handleSubscribe

--- app/components/mailing.tsx
+++ app/components/mailing.tsx
@@ -3,43 +3,48 @@ import React, { useState } from "react";
 import Button from "@/app/components/button";
 
 const Mailing: React.FC = () => {
-  const [isSubscribed, setIsSubscribed] = useState(false);
-  const [email, setEmail] = useState<string>('');
 
-  const handleSubscribe = async () => {
-    try {
-      setIsSubscribed(true);
-    } catch (error) {
-      console.error('Error subscribing to mailing list:', error);
-    }
+  const [subscribed, setSubscribed] = useState<boolean>(false);
+
+  const handleSubscribe = () => {
+    setSubscribed(true);
   };
 
   return (
-    <div>
+    <footer>
+      <div className="flex justify-center items-center mt-40 text-red-500 font-sans mb-4 text-xl">
+        Sign Up For YourBeat Mailing List
+      </div>
+
       <div>
         YourBeat is an exciting new music network platform with a strong commitment to personal data security. Sign up for our mailing list to get notified when the web app is deployed, and we'll be sure to let you know when you can create your profile and start connecting with music industry professionals or discovering awesome new artists!
       </div>
-      {!isSubscribed ? (
-        <>
-          <Button variant="green">Option 1</Button>
-          <Button variant="green">Option 2</Button>
-          <Button variant="green">Option 3</Button>
-          <Button variant="green">Option 4</Button>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Button variant="green" onClick={handleSubscribe}>
-            Subscribe
-          </Button>
-        </>
+
+      {subscribed ? (
+        <div className="text-center">
+          <p>Thank you for signing up!</p>
+        </div>
       ) : (
-        <p>Thank you for signing up!</p>
+        <>
+          <div className="flex flex-row justify-center">
+            <Button>Artist</Button>
+            <Button>Fan</Button>
+            <Button>Vendor</Button>
+            <Button>Venue</Button>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <input className="h-10 mt-4 pl-2 text-black" type="text" placeholder="Email Address" />
+          </div>
+
+          <div className="flex justify-center items-center mt-3">
+            <Button>Subscribe</Button>
+          </div>
+        </>
       )}
-    </div>
+    </footer>
   );
 };
+
 
 export default Mailing;
