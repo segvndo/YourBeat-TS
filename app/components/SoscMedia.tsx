e2822ddae67b6a9059f705ee24a766a4944d8416
Bea Mungcal <beamungcal@hotmail.com>
2023-12-27 02:43:28 -0500

    Modify social media icon positioning

--- app/components/SoscMedia.tsx
+++ app/components/SoscMedia.tsx
@@ -1,21 +1,33 @@
+'use client';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons';
-
+import { faInstagram, faDiscord, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 type SoscMediaProps = {};
 
 const SoscMedia: React.FC<SoscMediaProps> = () => {
-
   return (
     <div>
       Follow Us!
-      <div>
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faDiscord} />
+      <div className="icon-container">
+        <div>
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faDiscord} />
+          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon icon={faYoutube} />
+        </div>
       </div>
+      <style jsx>{`
+        .icon-container {
+          display: flex;
+          justify-content: space-around;  // Adjust as needed
+          margin-top: 10px;  // Adjust as needed
+        }
+
+        // Additional styles as needed
+      `}</style>
     </div>
-  )
+  );
 };
 
 export default SoscMedia;
