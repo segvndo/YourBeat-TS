c2ab99e42738721642e48998ecc13ff4cb65a2d6
Bea Mungcal <beamungcal@hotmail.com>
2023-12-27 02:25:36 -0500

    Add social media icons

--- app/components/SoscMedia.tsx
+++ app/components/SoscMedia.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 type SoscMediaProps = {};
 
 const SoscMedia: React.FC<SoscMediaProps> = () => {
 
   return (
-    <div>Follow Us!</div>
+    <div>
+      Follow Us!
+      <div>
+        <FontAwesomeIcon icon={faFacebook} />
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faInstagram} />
+      </div>
+    </div>
   )
 };
 
