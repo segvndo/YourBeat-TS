e152e130c2231f687f6faec65f352be433292f12
Bea Mungcal <beamungcal@hotmail.com>
2023-12-27 03:44:31 -0500

    Add logo text in homeheading component

--- app/components/HomeHeading.tsx
+++ app/components/HomeHeading.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 
 import YBLogo from '@/app/images/YBLogo.png';
+import YBText from '@/app/images/YBText.png';
 
 const HomeHeading: React.FC = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold">Welcome to YourBeat!</h1>
       {/* Use the Next.js Image component */}
-      <Image src={YBLogo} alt="YourBeat Logo" width={200} height={100} className="mt-4" />
+      <Image src={YBLogo} alt="YourBeat Logo" width={600} height={700} className="mt-4" />
+      <Image src={YBText} alt="YourBeat Text" width={600} height={700} className="mt-4" />
 
     </div>
   );
