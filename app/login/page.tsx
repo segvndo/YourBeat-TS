42acb831bb0dc58985452cda53aab685fb34196b
Bea Mungcal <beamungcal@hotmail.com>
2024-01-01 23:13:51 -0500

    Create initial login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
+type LoginProps = {};
+
 const Login: React.FC<LoginProps> = () => {
   return (
     <div>
-      <p>Login</p>
+      <h1>Login</h1>
 
       <form>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" id="email" />
 
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" id="password" />
+
+        <button type="submit" className="rounded-lg">Submit</button>
       </form>
     </div>
   )
