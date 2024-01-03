2ac2909a079f1a8fc133060c5ccfc4d49292e26d
Bea Mungcal <beamungcal@hotmail.com>
2024-01-02 22:39:21 -0500

    Adjust auth form position

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -26,7 +26,7 @@ const Login: React.FC<LoginProps> = () => {
   return (
     <div className="auth-form-container">
       <h2 className="text-3xl flex flex-center justify-center mb-4">Login</h2>
-      <div className="login-form flex flex-col">
+      <div className="login-form flex flex-col items-center">
         <label htmlFor="username">Username</label>
         <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" id="username" name="username" className="box-container h-8 w-24" />
         <label htmlFor="password">Password</label>
